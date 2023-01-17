7593f215f9d9fda5fda1e20e026a12aa060d089d
Chahat <chahat_sharma@vibrantstack.com>
2023-01-17 22:32:26 +0530

    update

--- blogp/myblog/views.py
+++ blogp/myblog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import blog
-
+from django.core.paginator import Paginator
 # Create your views here.
 def home(request):
     # my_blogs=get_object_or_404(blog,pk=1)
     my_blogs=blog.objects.all()
+    p = Paginator(my_blogs,3)
+    page = request.GET.get('page')
+    page_obj = p.get_page(page)
    
-    return render (request,'myblog/index.html',{'blog': my_blogs},)
+    return render (request,'myblog/index.html',{'blog': my_blogs, 'page_obj':page_obj})
 
 def article(request,blog_id):
     my_blogs=get_object_or_404(blog,pk=blog_id)
